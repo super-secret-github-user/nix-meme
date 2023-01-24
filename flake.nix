9ec6b463f3b2cc2fd83486604932b53bad138490
Edward Gokmen <edward.gokmen@helsing.ai>
2023-01-24 17:23:00 +0000

    slowly patching deps

--- flake.nix
+++ flake.nix
@@ -25,10 +25,32 @@
           buildInputs = [
             (pkgs.poetry2nix.mkPoetryApplication {
               projectDir = krakenw.outPath;
+	      overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend ( self: super: {
+	        nr-io-lexer = super.nr-io-lexer.overridePythonAttrs
+	          ( old: {
+                      buildInputs = (old.buildInputs or [ ]) ++ [ super.poetry ];
+                    }); 
+	        types-termcolor = super.types-termcolor.overridePythonAttrs
+	          ( old: {
+                      buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                    }); 
+	        builddsl = super.builddsl.overridePythonAttrs
+	          ( old: {
+                      buildInputs = (old.buildInputs or [ ]) ++ [ super.poetry ];
+                    }); 
+	        iniconfig = super.iniconfig.overridePythonAttrs
+	          ( old: {
+                      buildInputs = (old.buildInputs or [ ]) ++ [ super.hatchling ];
+                    }); 
+	        kraken-common = super.kraken-common.overridePythonAttrs
+	          ( old: {
+                      buildInputs = (old.buildInputs or [ ]) ++ [ super.hatchling ];
+                    }); 
+	      });
             })
-            (pkgs.poetry2nix.mkPoetryApplication {
-              projectDir = slap-cli.outPath;
-            })
+            # (pkgs.poetry2nix.mkPoetryApplication {
+            #   projectDir = slap-cli.outPath;
+            # })
           ];
         };
       }
