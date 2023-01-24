2d69d31b68e03b47f21cca59d18f30f16251ceff
Edward Gokmen <edward.gokmen@helsing.ai>
2023-01-24 16:55:01 +0000

    cross platform

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    flake-utils.url = "github:numtide/flake-utils";
     slap-cli = {
       url = "github:super-secret-github-user/slap";
       flake = false;
@@ -14,26 +14,23 @@
     };
   };
 
-  outputs =
-
-  { self, nixpkgs, poetry2nix, slap-cli, krakenw }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      shell =
-      pkgs.mkShell
+  outputs = { self, nixpkgs, poetry2nix, slap-cli, krakenw, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
       {
-        name = "kraken-wrapper";
-        buildInputs = [
-          (pkgs.poetry2nix.mkPoetryApplication {
-            projectDir = krakenw.outPath;
-          })
-          (pkgs.poetry2nix.mkPoetryApplication {
-            projectDir = slap-cli.outPath;
-          })
-        ];
-      };
-  in
-  {
-    devShells.x86_64-linux.default = shell;
-  };
+        devShells.default =  pkgs.mkShell {
+          name = "kraken-wrapper";
+          buildInputs = [
+            (pkgs.poetry2nix.mkPoetryApplication {
+              projectDir = krakenw.outPath;
+            })
+            (pkgs.poetry2nix.mkPoetryApplication {
+              projectDir = slap-cli.outPath;
+            })
+          ];
+        };
+      }
+    );
 }
