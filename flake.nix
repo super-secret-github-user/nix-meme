fee42264a448f6ae5807fc5d5a6642aa9b31ce0e
Edward Gokmen <edward.gokmen@helsing.ai>
2023-01-24 15:18:20 +0000

    moment of truth

--- flake.nix
+++ flake.nix
@@ -3,13 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    slap-cli.url = "github:NiklasRosenstein/slap";
-    krakenw.url = "github:/kraken-build/kraken-wrapper";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    slap-cli = {
+      url = "github:super-secret-github-user/slap";
+      flake = false;
+    };
+    krakenw = {
+      url = "github:/helsing-ai/kraken-wrapper";
+      flake = false;
+    };
   };
 
   outputs =
 
-  { self, nixpkgs, slap-cli, krakenw }:
+  { self, nixpkgs, poetry2nix, slap-cli, krakenw }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       shell =
